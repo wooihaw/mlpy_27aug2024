0420a73f109e3f68f3124484883f64a402346260
wooihaw <wooihaw.tan@gmail.com>
2024-08-26 22:35:30 +0800

    Initial commit

---
+++ ml26.py
@@ -0,0 +1,11 @@
+# SVM Classification
+from pandas import read_csv
+from sklearn.model_selection import train_test_split as split
+from sklearn.svm import SVC
+header = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+df = read_csv('data/pima-indians-diabetes.data.csv', names=header)
+X = df.drop(columns=['class'])
+y = df['class']
+X_train, X_test, y_train, y_test = split(X, y, test_size=0.25, random_state=42)
+svc = SVC().fit(X_train, y_train)
+print(f'Accuracy: {svc.score(X_test, y_test):.2%}')
